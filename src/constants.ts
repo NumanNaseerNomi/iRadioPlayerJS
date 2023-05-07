4b3095ae29d191c5f989c5f83c491dc76a76a7d3
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2023-05-07 23:56:26 +0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,42 @@
 /**
  * @public
  */
+export const ApiUrl = 'http://all.api.radio-browser.info/json/servers';
+
+/**
+ * @public
+ */
+export const ServersList = 
+[
+  {
+      "ip": "2a0a:4cc0:0:db9:282b:91ff:fed0:ddea",
+      "name": "at1.api.radio-browser.info"
+  },
+  {
+      "ip": "2a03:4000:37:42:c4fe:4cff:fea7:8941",
+      "name": "de1.api.radio-browser.info"
+  },
+  {
+      "ip": "2001:19f0:5001:32a4:5400:2ff:fe37:75c2",
+      "name": "nl1.api.radio-browser.info"
+  },
+  {
+      "ip": "89.58.16.19",
+      "name": "v22022045963187310.megasrv.de"
+  },
+  {
+      "ip": "91.132.145.114",
+      "name": "de1.api.radio-browser.info"
+  },
+  {
+      "ip": "95.179.139.106",
+      "name": "nl1.api.radio-browser.info"
+  }
+];
+
+/**
+ * @public
+ */
 export const StationSearchOrder = {
   name: 'name',
   url: 'url',
